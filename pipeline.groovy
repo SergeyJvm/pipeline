727ced4d456bc13f994b71039e968b200583e492
MsiPK <extreme_0788@bk.ru>
2023-09-14 07:58:29 +0300

    Add steps

--- pipeline.groovy
+++ pipeline.groovy
@@ -9,14 +9,17 @@ pipeline {
     stages {
 
         stage("Download idea community") {
-            dir(sberide) {
-                script {
-                    //clean workspace
-                    cleanWs disableDeferredWipeout: true, deleteDirs: true
-                    //download
-                    git branch: 'main', credentialsId: 'extreme_0788@bk.ru', url: 'https://github.com/JetBrains/intellij-community.git'
-                    sh "ls -la"
+            steps {
+                sh "mkdir -p sberide/android"
+                dir(sberide) {
+                    script {
+                        //clean workspace
+                        cleanWs disableDeferredWipeout: true, deleteDirs: true
+                        //download
+                        git branch: 'main', credentialsId: 'extreme_0788@bk.ru', url: 'https://github.com/JetBrains/intellij-community.git'
+                        sh "ls -la"
 
+                    }
                 }
             }
         }
