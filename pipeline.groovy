c197253359c84e050d05cdef34a9fcf3c4f2453e
MsiPK <extreme_0788@bk.ru>
2023-09-14 07:55:05 +0300

    Add pipeline for idea

---
+++ pipeline.groovy
@@ -0,0 +1,26 @@
+pipeline {
+
+    agent {
+        node {
+            label "Linux_Default"
+        }
+    }
+
+    stages {
+
+        stage(Download idea community) {
+            sh "mkdir -p sberide/android"
+            dir(sberide) {
+                script {
+                    //clean workspace
+                    cleanWs disableDeferredWipeout: true, deleteDirs: true
+                    //download
+                    git branch: 'main', credentialsId: 'extreme_0788@bk.ru', url: 'https://github.com/JetBrains/intellij-community.git'
+                    sh "ls -la"
+
+                }
+            }
+        }
+    }
+
+}
